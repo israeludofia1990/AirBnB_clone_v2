a473107bbc28a3e2a5dcc06be37f8eea1b5fa348
udofiaisrael1990 <udofiaisrael170@gmail.com>
2024-01-13 15:15:48 +0000

    3-deploy_web_static.py

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Fabric script based on the file 2-do_deploy_web_static.py that creates and
+distributes an archive to the web servers
+"""
+
+
+from fabric.api import env, local, put, run
+from datetime import datetime
+from os.path import exists, isdir
+from 2-do_deploy_web_static.py import do_deploy
+from 1-pack_web_static.py import do_pack
+
+
+env.hosts = ['100.24.253.52', '52.86.163.192']
+
+
+def deploy():
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
