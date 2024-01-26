862ccfe97a36fed7a6c4fbfbf7985a10c5a17a3d
udofiaisrael1990 <udofiaisrael170@gmail.com>
2024-01-26 12:27:23 +0000

    first commit

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,7 +24,7 @@ def text(text=None):
     return "C {}".format(text.replace('_', ' '))
 
 
-@app.route('/python/<text>', strict_flash=False)
+@app.route('/python/<text>', strict_slashes=False)
 def python_route(text='is cool'):
     '''display “Python ”, followed by the value of the text variable'''
     return 'Python {}'.format(text.replace('_', ' '))
