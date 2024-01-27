763aea5ec5128175a957314b24f0f4a51a4ba09f
udofiaisrael1990 <udofiaisrael170@gmail.com>
2024-01-27 03:11:07 -0800

    second commit

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+'''script that starts a Flask web application'''
+from flask import Flask, render_template
+
+
+app = Flask = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Hello_nbnb():
+    '''default route'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    '''hbnb_route'''
+    return 'HBNB'
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def text(text=None):
+    '''Dynamic inputed text: replace _ for space and show text'''
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text='is cool'):
+    '''display “Python ”, followed by the value of the text variable'''
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def integer_route(n=None):
+    '''integer route'''
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def render_number(n=None):
+    """Display a HTML page only if n is an integer"""
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
